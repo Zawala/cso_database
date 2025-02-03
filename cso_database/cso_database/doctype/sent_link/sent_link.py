dd583e1a1f989baddb4495f360c2bfbaba3edfce
Kelvin zawala <87924723+Zawala@users.noreply.github.com>
2025-02-03 12:30:27 +0200

    working email link

--- cso_database/cso_database/doctype/sent_link/sent_link.py
+++ cso_database/cso_database/doctype/sent_link/sent_link.py
@@ -7,28 +7,20 @@ from frappe.model.document import Document
 
 class SentLink(Document):
     def send_link_email(self):
-        # email, name, link=frappe.db.get_value("SentLink", {'userid':self.userid}, ['email' , "name1", "link"])
-        message = """
-            Dear %s,
-                    
-            Please Upload Organisation data on, %s To Allow the public to view your org.
-                    
-   
-            Thank you.
-            """%(
-                self.userid,
-                self.link
-            )
-            
-        frappe.sendmail(
-                recipients=[self.email],
-                expose_recipients = 'header',
-                subject='Fill in data for Zimcsodatabase',
-                as_markdown=True,
-                message=message,
-                header=('Upload Data!')
-            )
+        message = f"""
+    <p>Dear {self.userid},</p>
+    <p>Please upload organisation data on <a href="{self.link}">{self.link}</a> to allow the public to view your org.</p>
+    <p>Thank you.</p>
+    """
 
+    # Send the email
+        frappe.sendmail(
+            recipients=[self.email],
+            expose_recipients='header',
+            subject='Fill in data for Zimcsodatabase',
+            message=message,  # HTML content
+            header='Upload Data!'
+        )
 
 @frappe.whitelist()
 def sendmail(name):
